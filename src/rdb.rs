91ed5fdaa364a8b949a860d84a8479eff976f926
Bartosz Grabias <bartosz.grabias@dxc.com>
2023-12-22 18:07:32 +0100

    Implement another test

--- src/rdb.rs
+++ src/rdb.rs
@@ -370,4 +370,34 @@ mod test {
             Some(&OwnedValue::String("blueberry".into()))
         );
     }
+
+    #[test]
+    fn test_muliple_values() {
+        let data = vec![
+            82, 69, 68, 73, 83, 48, 48, 48, 51, 250, 9, 114, 101, 100, 105, 115, 45, 118, 101, 114,
+            5, 55, 46, 50, 46, 48, 250, 10, 114, 101, 100, 105, 115, 45, 98, 105, 116, 115, 192,
+            64, 254, 0, 251, 4, 0, 0, 6, 98, 97, 110, 97, 110, 97, 5, 103, 114, 97, 112, 101, 0, 9,
+            114, 97, 115, 112, 98, 101, 114, 114, 121, 9, 114, 97, 115, 112, 98, 101, 114, 114,
+            121, 0, 5, 109, 97, 110, 103, 111, 6, 111, 114, 97, 110, 103, 101, 0, 6, 111, 114, 97,
+            110, 103, 101, 6, 98, 97, 110, 97, 110, 97, 255, 83, 61, 20, 90, 34, 123, 49, 126, 10,
+        ];
+
+        let parsed = Database::parse(&data).expect("data is valid, parsing should succeed");
+        assert_eq!(
+            parsed.keys().get("banana"),
+            Some(&OwnedValue::String("grape".into()))
+        );
+        assert_eq!(
+            parsed.keys().get("raspberry"),
+            Some(&OwnedValue::String("raspberry".into()))
+        );
+        assert_eq!(
+            parsed.keys().get("mango"),
+            Some(&OwnedValue::String("orange".into()))
+        );
+        assert_eq!(
+            parsed.keys().get("orange"),
+            Some(&OwnedValue::String("banana".into()))
+        );
+    }
 }
